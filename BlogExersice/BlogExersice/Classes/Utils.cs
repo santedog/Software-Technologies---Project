bc93a1050f77430708cf01be54d8f0cb0e020b73
Stoyan Tenev <te4o_21@abv.bg>
2016-08-23 23:39:34 +0300

    created
    created from tutorial

---
+++ BlogExersice/BlogExersice/Classes/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogExersice.Classes
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLength = 100)
+        {
+            if (text == null || text.Length <= maxLength)
+                return text;
+            var shortText = text.Substring(0, maxLength) + "...";
+            return shortText;
+        }
+
+    }
+}
---
+++ BlogExersice/BlogExersice/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using BlogExersice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogExersice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var post = db.Posts.OrderByDescending(p => p.Date).Take(3);
+            return View(post.ToList());
+        }
+
+    }
+}
