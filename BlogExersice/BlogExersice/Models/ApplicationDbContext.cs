fb52abc5a3166abcd5eee10d9685826291cfb237
Stoyan Tenev <te4o_21@abv.bg>
2016-08-28 15:03:29 +0300

    Add Comment clas
    DB Comment created with Post_id and Author_id

--- BlogExersice/BlogExersice/Models/ApplicationDbContext.cs
+++ BlogExersice/BlogExersice/Models/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace BlogExersice.Models
     {
         public DbSet<Post> Posts { get; set; }
 
+        public DbSet<Comment> Comments { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ BlogExersice/BlogExersice/Models/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogExersice.Models
+{
+    public class Comment
+    {
+        public Comment()
+        {
+            this.Date = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Body { get; set; }
+        
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+        public Post Post { get; set; }
+    }
+}
